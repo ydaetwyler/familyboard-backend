a55e2d7431e21347b5696563262ea3f22fbcb997
ydaetwyler <ydaetwyler@previon.ch>
2022-01-19 21:05:12 +0100

    send pw reset mail (gmx)

--- server/src/resolvers/services/lostPassword.mjs
+++ server/src/resolvers/services/lostPassword.mjs
@@ -1,8 +1,10 @@
 import { AuthenticationError } from 'apollo-server-express'
 import dotenv from 'dotenv'
+import nodemailer from 'nodemailer'
 
 dotenv.config()
 const FRONT_BASE_URL = process.env.FRONT_BASE_URL
+const mailPw = process.env.MAIL_PW
 
 const lostPassword = async (args, User) => {
     try {
@@ -20,7 +22,28 @@ const lostPassword = async (args, User) => {
             throw new AuthenticationError('Error user not found')
         }
 
-        console.log(`Send Mail -> ${FRONT_BASE_URL}/reset/${userFetched.hash}`)
+        const transport = nodemailer.createTransport({
+            host: "mail.gmx.net",
+            port: 587,
+            secure: false,
+            auth: {
+                user: "familyboard@gmx.ch",
+                pass: mailPw,
+            },
+        })
+
+        const mailOptions = {
+            from: 'familyboard@gmx.ch',
+            to: userEmail,
+            subject: 'Family Board reset pw',
+            text: `Click here to set a new password: ${FRONT_BASE_URL}/reset/${userFetched.hash}`
+        }
+
+        transport.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(`Error sending pw reset mail -> ${error}`)
+            } 
+        })
 
         return true
     } catch(e) {
