8dc6f74b9729924b823ee86350fbc32deda60ec3
ydaetwyler <ydaetwyler@previon.ch>
2022-01-19 21:01:49 +0100

    send invitation mail (gmx)

--- server/src/resolvers/services/invite.mjs
+++ server/src/resolvers/services/invite.mjs
@@ -1,9 +1,11 @@
 import { nanoid } from 'nanoid'
 import dotenv from 'dotenv'
+import nodemailer from 'nodemailer'
 
 dotenv.config()
 
 const frontBaseUrl = process.env.FRONT_BASE_URL
+const mailPw = process.env.MAIL_PW
 
 const invite = async (args, context, Family, User) => {  
     if (!context.isAuth) {
@@ -37,7 +39,32 @@ const invite = async (args, context, Family, User) => {
             family: updateFamily.id
         })
 
-        console.log(`${frontBaseUrl}/login/${user.hash}`)
+        const transport = nodemailer.createTransport({
+            host: "mail.gmx.net",
+            port: 587,
+            secure: false,
+            auth: {
+                user: "familyboard@gmx.ch",
+                pass: mailPw,
+            },
+        })
+
+        const currentUser = await User.findById({ _id: context.userId })
+        const senderEmail = currentUser.userEmail
+
+        const mailOptions = {
+            from: 'familyboard@gmx.ch',
+            to: email,
+            subject: 'Family Board invitation',
+            text: `You have been invited to join Family Board by ${senderEmail}. Click here to join: ${frontBaseUrl}/login/${user.hash}`
+        }
+
+        transport.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(`Error sending invitation mail -> ${error}`)
+            } 
+        })
+
     } catch (e) {
         console.log(`Error inviting User -> ${e}`)
         throw e
